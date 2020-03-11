e613bb12c1568257d79d627e2bb924bc8fe8fe6b
Jeson Rosario <jesonrosario22@gmail.com>
2020-03-11 15:00:12 -0400

    Updates

---
+++ precache-manifest.052eaea27e164ebec083bab49488a426.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7938818be1f4ce58fabbfe9d8700afd",
+    "url": "/favLinks/index.html"
+  },
+  {
+    "revision": "0713857f8a7b761183aa",
+    "url": "/favLinks/static/css/main.fc29d31f.chunk.css"
+  },
+  {
+    "revision": "356d58ac5d49e75f1d56",
+    "url": "/favLinks/static/js/2.95147f73.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/favLinks/static/js/2.95147f73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0713857f8a7b761183aa",
+    "url": "/favLinks/static/js/main.57a8d743.chunk.js"
+  },
+  {
+    "revision": "71a496425f051752a7f7",
+    "url": "/favLinks/static/js/runtime-main.bf8179a2.js"
+  }
+]);
